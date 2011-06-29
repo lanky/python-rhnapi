350e78f71d294176b77f6d5f108857a1969219ed
Stuart Sears <sjs@redhat.com>
2011-06-29 14:52:23 +0100

    updated distutils setup: * increased version to 5.4.0 (from 5.4) * added a long description (RPM %description)

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 # setup script for packaging the rhnapi module
 from distutils.core import setup
 setup(name = 'python-rhnapi',
-      version = '5.4',
+      version = '5.4.0',
       description = 'Python abstraction of the RHN Satellite XMLRPC API',
+      long_description = 'A Python abstraction of the RHN Satellite XMLRPC API, written to assist with accessing the API from python scripts. Much of the API has been collapsed into simple namespaces. This is under heavy development and some functionality may be limited.',
       author = 'Stuart Sears',
       author_email = 'sjs@redhat.com',
       packages = [ 'rhnapi' ],
+      license = 'GPL v2+'
       )
 
 

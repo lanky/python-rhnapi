989d1fc9260c609b0ef86bdd8f8d52e4a6f78fe0
Stuart Sears <stuart.sears@man.com>
2012-01-30 14:52:30 +0000

    Updates for 5.4.1 * setup.py - updated 'version' to 5.4.1 * setup.cfg - reset 'release' to 1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # setup script for packaging the rhnapi module
 from distutils.core import setup
 setup(name = 'python-rhnapi',
-      version = '5.4.0',
+      version = '5.4.1',
       description = 'Python abstraction of the RHN Satellite XMLRPC API',
       long_description = 'A Python abstraction of the RHN Satellite XMLRPC API, written to assist with accessing the API from python scripts. Much of the API has been collapsed into simple namespaces. This is under heavy development and some functionality may be limited.',
       author = 'Stuart Sears',
